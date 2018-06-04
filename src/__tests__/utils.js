41eb7bc4bfbf97855f71cde095bb6878be16f479
Justin Williams <justinwilliams42@gmail.com>
2018-06-04 14:40:30 -0700

    add tests

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -1,25 +1,68 @@
-import { decimalToDMS, parseDMS } from '../utils'
+import data from '../__mocks__/data'
+import {
+  decimalToDMS,
+  dmsToDecimal,
+  normalizeInput,
+  parseDMS,
+  validateDMS,
+} from '../utils'
 
-const pairs = [
-  [90, '90.00.00N'],
-  [-90, '90.00.00S'],
-  [30.263888889, '30.15.50N'],
-]
+/**
+ * Data format:
+ * [
+ *   [
+ *     '<DMS>',
+ *     '<normalizedDMS>',
+ *     [[<D>, <M>, <S>, '<N|S>'], [<D>, <M>, <S>, '<E|W>']],
+ *     [<latDD>, <lonDD>]
+ *   ]
+ * ]
+ */
 
-/*describe('dmsToDecimal', () => {
-  pairs.forEach(item => {
-    const dms = parseDMS(item[1])
-
-    it(`${item[1]} -> ${item[0]}`, () => {
-      expect(dmsToDecimal(dms)).toBe(item[0])
+describe('normalizeInput', () => {
+  data.forEach(item => {
+    it(`${item[0]} -> ${item[1]}`, () => {
+      expect(normalizeInput(item[0])).toBe(item[1])
     })
   })
-})*/
+})
+
+describe('validateDMS', () => {
+  data.forEach(item => {
+    it(`${item[1]}`, () => {
+      expect(validateDMS(item[1])).toBe(true)
+    })
+  })
+})
+
+describe('parseDMS', () => {
+  data.forEach(item => {
+    it(`${item[1]} -> ${item[2]}`, () => {
+      expect(parseDMS(item[1])).toEqual(item[2])
+    })
+  })
+})
+
+describe('dmsToDecimal', () => {
+  data.forEach(item => {
+    it(`Latitude: ${item[2][0]} -> ${item[3][0]}`, () => {
+      expect(dmsToDecimal(...item[2][0])).toBe(item[3][0])
+    })
+
+    it(`Longitude: ${item[2][1]} -> ${item[3][1]}`, () => {
+      expect(dmsToDecimal(...item[2][1])).toBe(item[3][1])
+    })
+  })
+})
 
 describe('decimalToDMS', () => {
-  pairs.forEach(item =>
-    it(`${item[0]} -> ${item[1]}`, () => {
-      expect(decimalToDMS(item[0], item[2])).toBe(item[1])
+  data.forEach(item => {
+    it(`Latitude: ${item[3][0]} -> ${item[2][0]}`, () => {
+      expect(decimalToDMS(item[3][0], false)).toEqual(item[2][0])
     })
-  )
+
+    it(`Longitude: ${item[3][1]} -> ${item[2][1]}`, () => {
+      expect(decimalToDMS(item[3][1], true)).toEqual(item[2][1])
+    })
+  })
 })
