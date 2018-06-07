7acea43d6f811962d524512627d9db37e0d33902
Justin Williams <justinwilliams42@gmail.com>
2018-06-07 14:00:22 -0700

    add tests

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -1,9 +1,12 @@
 import data from '../__mocks__/data'
 import {
+  convertInput,
   decimalToDMS,
   dmsToDecimal,
+  fill,
   normalizeInput,
   parseDMS,
+  serializeDMS,
   validateDMS,
 } from '../utils'
 
@@ -19,10 +22,29 @@ import {
  * ]
  */
 
+const dmsPrecision = 3
+
+describe('fill', () => {
+  it(`should equal`, () => {
+    expect(fill([], 'x', 3)).toEqual(['x', 'x', 'x'])
+  })
+  it(`should equal`, () => {
+    expect(fill(['x'], 'o', 1)).toEqual(['x', 'o'])
+  })
+})
+
 describe('normalizeInput', () => {
   data.forEach(item => {
     it(`${item[0]} -> ${item[1]}`, () => {
       expect(normalizeInput(item[0])).toBe(item[1])
+    })
+  })
+})
+
+describe('convertInput', () => {
+  data.forEach(item => {
+    it(`${item[3]} -> ${item[1]}`, () => {
+      expect(convertInput(item[3].join(','), dmsPrecision)).toBe(item[1])
     })
   })
 })
@@ -43,6 +65,14 @@ describe('parseDMS', () => {
   })
 })
 
+describe('serializeDMS', () => {
+  data.forEach(item => {
+    it(`${item[2]} -> ${item[1]}`, () => {
+      expect(serializeDMS(item[2][0], item[2][1])).toEqual(item[1])
+    })
+  })
+})
+
 describe('dmsToDecimal', () => {
   data.forEach(item => {
     it(`Latitude: ${item[2][0]} -> ${item[3][0]}`, () => {
@@ -58,11 +88,11 @@ describe('dmsToDecimal', () => {
 describe('decimalToDMS', () => {
   data.forEach(item => {
     it(`Latitude: ${item[3][0]} -> ${item[2][0]}`, () => {
-      expect(decimalToDMS(item[3][0], false, 3)).toEqual(item[2][0])
+      expect(decimalToDMS(item[3][0], false, dmsPrecision)).toEqual(item[2][0])
     })
 
     it(`Longitude: ${item[3][1]} -> ${item[2][1]}`, () => {
-      expect(decimalToDMS(item[3][1], true, 3)).toEqual(item[2][1])
+      expect(decimalToDMS(item[3][1], true, dmsPrecision)).toEqual(item[2][1])
     })
   })
 })
