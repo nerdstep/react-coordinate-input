bb24251126de777e096fe62c6108096da9f61c6d
Justin Williams <justinwilliams42@gmail.com>
2020-05-13 08:30:07 -0700

    chore: update imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url'
 import filesize from 'rollup-plugin-filesize'
-import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import url from 'rollup-plugin-url'
 import pkg from './package.json'
 
 export default {
